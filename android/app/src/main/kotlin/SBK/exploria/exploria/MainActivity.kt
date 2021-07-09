4c3c9130f8ce75806e655817339280f3b07fc4a9
chiragsardana <30770247+chiragsardana@users.noreply.github.com>
2021-07-09 09:56:13 +0530

    Basic UI For Project

---
+++ android/app/src/main/kotlin/SBK/exploria/exploria/MainActivity.kt
@@ -0,0 +1,6 @@
+package SBK.exploria.exploria
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:exploria/pages/login_page.dart';
+import 'package:exploria/utils/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: MyRoutes.loginRoute,
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage()
+      },
+    );
+  }
+}
